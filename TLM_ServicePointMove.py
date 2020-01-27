0935cb299013cae19ea2e29254e19bbf13984b16
Erin <39073850+miyata710@users.noreply.github.com>
2020-01-27 13:37:44 -0500

    Update TLM_ServicePointMove.py

--- TLM_ServicePointMove.py
+++ TLM_ServicePointMove.py
@@ -1,37 +1,41 @@
-def temp_move(a):#sp move
-    workspace = r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde'
+'''
+These functions were created to aid in the the reconstruction of production level data that was altered back to its original
+geographic position.
+'''
+
+def temp_move(i):#sp move
+    workspace = #SDE connection file
     edit = arcpy.da.Editor(workspace)
     edit.startEditing(False, True)
     edit.startOperation()
     
-    where="DEVICELOCATION={}".format(a)
-    cursor=arcpy.da.SearchCursor(r'E:\Data\EROlson\temp_091601.gdb\ServicePoint',["SHAPE@"],where)
+    SQL ="DEVICELOCATION={}".format(i)
+    cursor=arcpy.da.SearchCursor('''#path to archived service point FC''',["SHAPE@"],SQL)
     for row in cursor:
         sp_091601=row[0]        
         
-    where="DEVICELOCATION={}".format(a)
-    cursor=arcpy.da.UpdateCursor(r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["SHAPE@"],where)
+    SQL="DEVICELOCATION={}".format(i)
+    cursor=arcpy.da.UpdateCursor('''#path to existing service point FC in production''',["SHAPE@"],SQL)
     for row in cursor:
         row[0]=sp_091601
         cursor.updateRow(row)
     edit.stopOperation()
 
 
-def tlm_move(a):
-    workspace = r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde'
+def tlm_move(i):
+    workspace = #SDE connection file
     edit = arcpy.da.Editor(workspace)
     edit.startEditing(False, True)
     edit.startOperation()
     
-    where="TLM='{}'".format(a)
-    cursor=arcpy.da.SearchCursor(r'E:\Data\EROlson\temp_091601.gdb\Transformers',["SHAPE@"],where)
+    SQL="TLM='{}'".format(i)
+    cursor=arcpy.da.SearchCursor('''#path to archived transformer FC''',["SHAPE@"],SQL)
     for row in cursor:
         tlm_091601=row[0]        
         
-    where="TLM='{}'".format(a)
-    cursor=arcpy.da.UpdateCursor(r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde\ELECDIST.ElectricDist\ELECDIST.Transformer',["SHAPE@"],where)
+    SQL="TLM='{}'".format(i)
+    cursor=arcpy.da.UpdateCursor('''#path to existing transformer FC in production''',["SHAPE@"],SQL)
     for row in cursor:
         row[0]=tlm_091601
-        cursor.updateRow(row)
-        
+        cursor.updateRow(row)  
     edit.stopOperation()
