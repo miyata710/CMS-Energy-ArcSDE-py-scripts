48ed4308deb3ec5c89a8a95b12baf95bb2395f05
Erin <39073850+miyata710@users.noreply.github.com>
2020-04-03 15:54:04 -0400

    Update streetlightDict.py

--- streetlightDict.py
+++ streetlightDict.py
@@ -1,1 +1,21 @@
+'''
+this block of code retrieves a list of feederIDs that need to have their streetlight's traced
+'''
+circuitBound = r'Org Bounds\Circuit Boundaries'
+hqList = ['FRE', 'GVL', 'TRA', 'JAC', 'LAN', 'BEN','KAL','LDG','GRE', 'MDL', 'BIG','CLR','BCY','FLT','HST','SAG','BNC','MUS','GRA',
+'HML','CAD','WBR','UNK','BCK','TWS','OWS','GRN']
 
+#for storing a list of every single feederID we need to conduct trace on
+feederList = []
+
+for i in hqList:
+  SQL = """HDQ = '{}'""".format(i)
+  #!print SQL
+  cursor = arcpy.da.SearchCursor(circuitBound, "FEEDERID", SQL) 
+  for row in cursor:
+    feederList.append(row)
+  del cursor
+  
+print len(feederList)
+  
+  
