4678432c8aaae11e7cf6d71769ec0f31bb340ba0
Erin <39073850+miyata710@users.noreply.github.com>
2020-04-04 12:52:05 -0400

    Update streetlightDict.py

--- streetlightDict.py
+++ streetlightDict.py
@@ -1,6 +1,6 @@
-'''
-this block of code retrieves a list of feederIDs that need to have their streetlight's traced
-'''
+
+#this block of code retrieves a list of feederIDs that need to have their streetlight's traced
+
 circuitBound = r'Org Bounds\Circuit Boundaries'
 hqList = ['FRE', 'GVL', 'TRA', 'JAC', 'LAN', 'BEN','KAL','LDG','GRE', 'MDL', 'BIG','CLR','BCY','FLT','HST','SAG','BNC','MUS','GRA',
 'HML','CAD','WBR','UNK','BCK','TWS','OWS','GRN']
@@ -17,5 +17,45 @@ for i in hqList:
   del cursor
   
 print len(feederList)
+
+#creat new clean list
+newList = []
+for j in feederList:
+  j = j[-1]
+  j = str(j)
+  newList.append(j)
+
+
+#Now we need to search the streetlight FC by feederID.
+#We will have a single dictionary where feederid is key and a list of streetlight OBJIDs associated with that feeder will be value.
+
+
+streetlight = r'Structures Group Layer\Streetlight'
+myDict = {}
+for k in newList:
+  SQL = """FEEDERID = '{}'""".format(k)
+  objList = []
+  cursor = arcpy.da.SearchCursor(streetlight, "OBJECTID", SQL) 
+  for row in cursor:
+    row = row[-1]
+    objList.append(row)
+  del cursor
   
+  myDict[k] = objList
   
+print len(myDict)
+
+### Need to update Streetlight FeederID field ###
+#feederID field is sub + circuit
+streetlight = r'Structures Group Layer\Streetlight'
+workHQ = r'Org Bounds\Electric Distribution WHQ(Scale<300,000)'
+
+hqList = ['FRE', 'GVL', 'TRA', 'JAC', 'LAN', 'BEN','KAL','LDG','GRE', 'MDL', 'BIG','CLR','BCY','FLT','HST','SAG','BNC','MUS','GRA',
+'HML','CAD','WBR','UNK','BCK','TWS','OWS','GRN']
+
+SQL = """FEEDERID IS NULL""" 
+cursor = arcpy.da.UpdateCursor(streetlight, ["FEEDERID","SUBSTATIONID","CIRCUITID"], SQL)
+for row in cursor:
+  row[0] = row[1] + row[2]
+  cursor.updateRow(row)
+del cursor
