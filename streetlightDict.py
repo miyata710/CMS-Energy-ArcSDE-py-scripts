fe7d14d6f557a7f1d6b41dbdc54351d6fd727fd3
Erin <39073850+miyata710@users.noreply.github.com>
2020-04-04 16:42:08 -0400

    Update streetlightDict.py

--- streetlightDict.py
+++ streetlightDict.py
@@ -16,7 +16,6 @@ for i in hqList:
     feederList.append(row)
   del cursor
   
-print len(feederList)
 
 #creat new clean list
 newList = []
@@ -24,7 +23,7 @@ for j in feederList:
   j = j[-1]
   j = str(j)
   newList.append(j)
-
+print newList
 
 #Now we need to search the streetlight FC by feederID.
 #We will have a single dictionary where feederid is key and a list of streetlight OBJIDs associated with that feeder will be value.
@@ -34,6 +33,7 @@ streetlight = r'Structures Group Layer\Streetlight'
 myDict = {}
 for k in newList:
   SQL = """FEEDERID = '{}'""".format(k)
+  
   objList = []
   cursor = arcpy.da.SearchCursor(streetlight, "OBJECTID", SQL) 
   for row in cursor:
@@ -43,19 +43,45 @@ for k in newList:
   
   myDict[k] = objList
   
-print len(myDict)
+print myDict
 
 ### Need to update Streetlight FeederID field ###
-#feederID field is sub + circuit
-streetlight = r'Structures Group Layer\Streetlight'
-workHQ = r'Org Bounds\Electric Distribution WHQ(Scale<300,000)'
-
-hqList = ['FRE', 'GVL', 'TRA', 'JAC', 'LAN', 'BEN','KAL','LDG','GRE', 'MDL', 'BIG','CLR','BCY','FLT','HST','SAG','BNC','MUS','GRA',
-'HML','CAD','WBR','UNK','BCK','TWS','OWS','GRN']
-
+#data
+streetlight = r'E:\Data\Streetlight_TLM\Data\PROD_DATABASE_04032020.gdb\ElectricDist\Streetlight'
+workHQ = r'E:\Data\ESME\Landbase.sde\GISLand.DBO.Land\GISLand.DBO.ElectricDistributionWHQ'
+#list of releavnt HQ
+#!hqList = ['FRE', 'GVL', 'TRA', 'JAC', 'LAN', 'BEN','KAL','LDG','GRE', 'MDL', 'BIG','CLR','BCY','FLT','HST','SAG','BNC','MUS','GRA',
+#!'HML','CAD','WBR','UNK','BCK','TWS','OWS','GRN']
+hqList = ['BIG','CLR','BCY','FLT','HST','SAG','BNC','MUS','GRA','HML','CAD','WBR','UNK','BCK','TWS','OWS','GRN']
+#streetlight SQL statement
 SQL = """FEEDERID IS NULL""" 
-cursor = arcpy.da.UpdateCursor(streetlight, ["FEEDERID","SUBSTATIONID","CIRCUITID"], SQL)
-for row in cursor:
-  row[0] = row[1] + row[2]
-  cursor.updateRow(row)
-del cursor
+for hq in hqList:
+  SQL1 = """BOUNDARYNAMECD = '{}'""".format(hq) 
+  
+  #make a layer of specific work HQ
+  hqLyr = arcpy.MakeFeatureLayer_management(workHQ,'in_memory\\hqData_lyr', SQL1)
+ 
+  # make a layer of streetlights w/ null feederID
+  lightLyr = arcpy.MakeFeatureLayer_management(streetlight,'in_memory\\lightData_lyr', SQL)
+  
+  #select by location
+  lightDataSel = arcpy.SelectLayerByLocation_management(lightLyr,"COMPLETELY_WITHIN",hqLyr,"","NEW_SELECTION")
+  
+  #update work HQ field in streetlight layer
+  #inputDataLyr OBJID's List
+  dataUpdateList = []
+
+  #loop through lightDataSel and append object IDs to list
+  with arcpy.da.SearchCursor(lightDataSel, ["OBJECTID"]) as cursor:
+    for row in cursor:
+      dataUpdateList.append(row[0])
+  
+  for j in dataUpdateList:  
+    with arcpy.da.UpdateCursor(streetlight, ["WORKHEADQUARTERS"], """OBJECTID ={0}""".format(j)) as cursor:
+      for row in cursor:
+        row[0] = hq
+        cursor.updateRow(row)
+  arcpy.Delete_management(hqLyr)
+  arcpy.Delete_management(lightLyr)
+  
+  
