b8a5544c4cb6c4b2ea91ae24ac82033055e4fcdf
Erin <39073850+miyata710@users.noreply.github.com>
2020-04-15 16:51:10 -0400

    Update tblxCalculateMeasuredLength.py

--- tblxCalculateMeasuredLength.py
+++ tblxCalculateMeasuredLength.py
@@ -1,72 +1,97 @@
 '''
-This script has been developed for the purpose of creating a streamlined, automation solution
-for calculating the values for the Measured Length & Length Source fields.
-
-Things to add:
-        1) Add workspace paramenter for function
-        2) Test as a tool and add necessary GetParameterAsText() variable assignment
-        3) Determine proper string formatting for assigning FC data paths to variables
-        ****Maybe remove "IS NULL" from SQL selection****
+This script has been developed for the purpose of calculating the values for the Measured Length & Length Source fields.
+####
+Issues:
+1) Do we need to include OH/UG connector lines?
+2) Should we be updating the "LENGTHSOURCE" field? Evidence this was being updated in one of Shelly's models.
 '''
+#Necessary modules 
+import arcpy
+import os
+
 #create function for calculating the value for "Measured Length" field
-def calculateML(feederID,dataPath):
+def calculateML(feederID,dataPath,userWorkspace):
+    #assign correct SQL statemnet
+    if dataPath == priOH:
+        SQL =  """(FEEDERID = '{0}') AND (SUBTYPECD <> 7)""".format(feederID)
+        
+    elif dataPath == priUG:
+        SQL =  """(FEEDERID = '{0}') AND (SUBTYPECD <> 7)""".format(feederID)
+
+    else:
+        SQL =  """(FEEDERID = '{0}')""".format(feederID)
+    
     searchFields = ["OBJECTID","SHAPE@LENGTH"]
     updateFields = ["OBJECTID","MEASUREDLENGTH","LENGTHSOURCE"]
     feederField = "FEEDERID"
     measureField = "MEASUREDLENGTH"
-    for feeder in feederID:
-        SQL = """{0} = '{1}'""".format(arcpy.AddFieldDelimiters(dataPath,feederField),feeder)
-        myDict = {}
-        searchCursor = arcpy.da.SearchCursor(dataPath,searchFields,SQL)
-        for row in searchCursor:
-            objID = row[0]
-            objLength =row[1]
-            myDict[objID] = objLength
-        del searchCursor
 
-        #set workspace
-        workspace = r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde'
-        
-        # Start an edit session. Must provide the worksapce.
-        edit = arcpy.da.Editor(workspace)
-        
-        # Edit session is started without an undo/redo stack for versioned data
-        
-        edit.startEditing(False, True)
-        
-        # Start an edit operation
-        edit.startOperation()
-        
-        updateCursor = arcpy.da.UpdateCursor(dataPath,updateFields,SQL)
-        for item in updateCursor:
-            myLength = myDict[item[0]]
-            item[1] = round(myLength*3.28084) #!just added round() function at Tierney's request. Not tested yet.
-            item[2] = "FM"
-            updateCursor.updateRow(item)
-        del updateCursor
-        
-        # Stop the edit operation.
-        edit.stopOperation()
-        
-        # Stop the edit session and save the changes
-        edit.stopEditing(True)
-        
-        
-#### Function Parameters ####
-feederID = [
-'009901',
-'009902',
-'051601',
-'051602'
-]
+    myDict = {}
+    searchCursor = arcpy.da.SearchCursor(dataPath,searchFields,SQL)
+    
+    for row in searchCursor:
+        objID = row[0]
+        objLength =row[1]
+        myDict[objID] = objLength
+    del searchCursor
 
-priOH = r'Primary Lines\Primary Overhead Conductor'
-priUG = r'Primary Lines\Primary Underground Conductor'
-secOH = r'Customers & Transformers\Secondary Overhead Conductor'
-secUG = r'Customers & Transformers\Secondary Underground Conductor'
+    #set workspace
+    workspace = userWorkspace
 
+    # Start an edit session. Must provide the worksapce.
+    edit = arcpy.da.Editor(workspace)
+
+    # Edit session is started without an undo/redo stack for versioned data
+
+    edit.startEditing(False, True)
+
+    # Start an edit operation
+    edit.startOperation()
+
+    updateCursor = arcpy.da.UpdateCursor(dataPath,updateFields,SQL)
+    for item in updateCursor:
+        myLength = myDict[item[0]]
+        item[1] = round(myLength*3.28084) #!added round() function at Tierney's request.
+        item[2] = "FM" #!still in question
+        updateCursor.updateRow(item)
+    del updateCursor
+
+    # Stop the edit operation.
+    edit.stopOperation()
+
+    # Stop the edit session and save the changes
+    edit.stopEditing(True)
+#### End function ####
+
+#### Get input from user ####
+# Script input parameters:
+#!sdeWorkspace = arcpy.GetParameterAsText (0) #SDE Connection file
+sdeWorkspace = r'E:\Data\EROlson\PROD_ DGSEP011AsEROlson.sde'
+#!txt_input = arcpy.GetParameterAsText (1) # .txt file with feederIDs
+
+#makes a list of feeder IDs from a TXT file
+feederList = ['051602']
+'''
+if txt_input :
+    fhand = open(txt_input)
+    for i in fhand:
+        i = i.strip()
+	feederList.append(str(i))
+    fhand.close()
+'''
+
+#Data Paths
+#!!! do we need to add OH/UG Connector lines to this script???
+#!!! SUBTYPECD <> 7
+priOH = "{0}\\ELECDIST.ElectricDist\\ELECDIST.PriOHElectricLineSegment".format(sdeWorkspace)
+#!!!SUBTYPECD <> 7
+priUG = "{0}\\ELECDIST.ElectricDist\\ELECDIST.PriUGElectricLineSegment".format(sdeWorkspace)
+secOH = "{0}\\ELECDIST.ElectricDist\\ELECDIST.SecOHElectricLineSegment".format(sdeWorkspace)
+secUG = "{0}\\ELECDIST.ElectricDist\\ELECDIST.SecUGElectricLineSegment".format(sdeWorkspace)
+
+#!dataList = [priOH, priUG, secOH, secUG]
+dataList = [priOH]
 #### Call and Execute calculate ML function on ALL necessary FCs####
-calculateML(feederID, priOH)
-calculateML(feederID, priUG)
-calculateML(feederID, secOH)
-calculateML(feederID, secUG)
+for feeder in feederList:
+    for data in dataList:
+        calculateML(feeder, data, sdeWorkspace)
