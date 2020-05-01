5d1103b911b844d10d0cfef1dda3eaf2ee23c562
Erin <39073850+miyata710@users.noreply.github.com>
2020-05-01 09:38:51 -0400

    Create tieSwitchTraceCsv.py

---
+++ tieSwitchTraceCsv.py
@@ -0,0 +1,9 @@
+#!{9: ['f', 7], 7: ['s', 4], 13: ['f', 113], 6: ['s', 2], 1: ['f', 28]}
+myDict = {9: ['f', 7], 7: ['s', 4], 13: ['f', 113], 6: ['s', 2], 1: ['f', 28]}
+#sort returned data into lists
+fOID = []
+sOID []
+dpdOID = []
+
+for key in myDict:
+    print key
